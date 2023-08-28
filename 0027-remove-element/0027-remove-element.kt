4dbc473a6d16c1afdc635379274f1181f5b06450
Adhitya Bayu <mr.adhityabayu@gmail.com>
2023-08-28 22:07:18 +0700

    Time: 168 ms (42.75%), Space: 34.4 MB (74.68%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.kt
@@ -0,0 +1,15 @@
+class Solution {
+
+  fun removeElement(nums: IntArray, `val`: Int): Int {
+    var i = 0
+    var j = 0
+    while (j < nums.size) {
+      if (nums[j] != `val`) {
+        nums[i] = nums[j]
+        i += 1
+      }
+      j += 1
+    }
+    return i
+  }
+}
