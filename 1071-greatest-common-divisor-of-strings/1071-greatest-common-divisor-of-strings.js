285a601fefd4bd33c2725e2fa4be5053a7f88bc1
Adhitya Bayu <mr.adhityabayu@gmail.com>
2024-05-30 13:56:21 +0700

    Time: 43 ms (95.29%), Space: 49.3 MB (47.20%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -5,22 +5,35 @@
  */
 var gcdOfStrings = function(str1, str2) {
   
+  //check fot the concatenation between the string
+  //example, if "ABCABC" + "ABC" is false so we can continue to process
+  
   if(str1 + str2 != str2 +str1){
     return "";
   }
   
+  //calculate the length between for each string
+  //"ABCABC" = 6
+  //"ABC" = 3
   let i = str1.length;
   let j = str2.length;
   
+  
+  //implementation of euclidean algorithm. takes two arguments
   let gcd = function(x, y){
+    
+    //if the y is equals to 0
+    //if y is zero, it means that x is divisible by y without a remainder
     if(!y){
       return x;
     }
     return gcd(y, x%y);
   }
   
+  //input for the testcase
   let k = gcd (i, j);
 
+  //returns a substring of str1 from the beginning to the kth character
   return str1.slice(0, k);
   
   
