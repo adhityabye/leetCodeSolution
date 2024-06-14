689fa48fe9b36d9567df83d4f94a03d3ad740a28
Adhitya Bayu <mr.adhityabayu@gmail.com>
2024-06-14 10:58:24 +0700

    Time: 63 ms (21.51%), Space: 51.2 MB (20.76%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,14 +3,7 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-  
-    //trim leading and trailing spacing
-    //replace the multiple scenarion spacing using regex *thanks to gpt idk wht it is
-    //split using delimiter " "
-    //reverse the word from array
-    //join the result array
-    let reversed = s.trim("  ", "").replace(/\s+/g, ' ').split(" ").reverse().join(" ")
-    
-    //return the result
-    return reversed
-};
+    let answer = s.trim().replace(/\s+/g, ' ').split(" ");
+    answer = answer.reverse();
+    return answer.join().replaceAll(',',' ')
+}
