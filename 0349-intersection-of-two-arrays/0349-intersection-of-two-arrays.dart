2d910d46c413ec2dbedfb9cd59a2743043e8065a
Adhitya Bayu <mr.adhityabayu@gmail.com>
2024-11-19 12:07:01 +0700

    Time: 20 ms (10.26%), Space: 150.3 MB (46.59%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.dart
@@ -0,0 +1,10 @@
+class Solution {
+  List<int> intersection(List<int> nums1, List<int> nums2) {
+    Set<int> set1 = Set.from(nums1);
+    Set<int> set2 = Set.from(nums2);
+
+    Set<int> intersect = set1.intersection(set2);
+
+    return intersect.toList();
+  }
+}
