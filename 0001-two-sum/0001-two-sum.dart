374502ccbe6458e3f9e088c0388393f54d90a585
Adhitya Bayu <mr.adhityabayu@gmail.com>
2025-01-08 13:00:49 +0700

    Time: 31 ms (55.97%), Space: 149.3 MB (56.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.dart
+++ 0001-two-sum/0001-two-sum.dart
@@ -1,12 +1,22 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    Map<int, int> numInd = {};
+
+    //initiate Map buat nyimpen pair key-value dengan Map kosong
+    Map<int, int> numId ={};
+
+    //iterate 
     for(int i = 0; i < nums.length; i++){
+        //cari pasangan komplemen dari target dan value at index i
         int complement = target - nums[i];
-        if(numInd.containsKey(complement)){
-            return [numInd[complement]!, i];
+        if(numId.containsKey(complement)){
+
+            //bakalan return pair value sebelumnya dan index sekarang
+            //pake null safety '!'
+            return [numId[complement]!, i];
         }
-        numInd[nums[i]] = i;
+
+        //assign key-value dari index and index-value sekarang
+        numId[nums[i]] = i;
     }
     return [];
   }
